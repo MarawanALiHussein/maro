e31bc9d7c9d9f0b64374fe104c930861728c428f
MarawanAli <maro01122134381@gmail.com>
2022-07-07 23:32:15 +0200

    first commit!

---
+++ portfolio/models.py
@@ -0,0 +1,16 @@
+from email.mime import image
+from operator import mod
+from statistics import mode
+from turtle import title
+from django.db import models
+
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title 
